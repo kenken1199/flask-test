7907d8432d06489c22d9b3d4d8613dca499c8f29
kenken1199 <47364493+kenken1199@users.noreply.github.com>
2021-03-26 07:09:31 +0900

    Create test-flaskhello.py

---
+++ test-flaskhello.py
@@ -0,0 +1,18 @@
+import unittest
+
+import flaskhello
+
+
+class TestFlaskHello(unittest.TestCase):
+
+    def setUp(self):
+        self.app = flaskhello.app.test_client()
+
+    def test_get(self):
+        response = self.app.get('/')
+        assert response.status_code == 200
+        assert response.data == 'Hello, World!'
+
+
+if __name__ == '__main__':
+    unittest.main()
