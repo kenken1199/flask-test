97b5a57060b96aae90b501636a9b4220a09a6320
kenken1199 <47364493+kenken1199@users.noreply.github.com>
2021-03-27 02:29:26 +0900

    Update flaskhello.py

--- flaskhello.py
+++ flaskhello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-    return 'Hello, World!', 200
+    return 'Hello, circleci', 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
